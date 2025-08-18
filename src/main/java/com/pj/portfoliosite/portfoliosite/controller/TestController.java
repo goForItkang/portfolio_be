aca1b663f1f98d6d648babaffce6008f1d85fa92
goForItkang <kangyohan.dev.0421@gmail.com>
2025-08-19 04:21:58 +0900

    project -update

--- src/main/java/com/pj/portfoliosite/portfoliosite/controller/TestController.java
+++ src/main/java/com/pj/portfoliosite/portfoliosite/controller/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    //ㅔㅌ스트 수정
+    //ㅔㅌ스트 수정dd
     @GetMapping("/")
     public HttpEntity<String> index(){
         String response = "Hello World";
