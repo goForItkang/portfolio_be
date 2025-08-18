fe1b3cfbcea62862c315ddb2055c270324fdba50
goForItkang <kangyohan.dev.0421@gmail.com>
2025-08-18 19:53:54 +0900

    test- yh

--- src/main/java/com/pj/portfoliosite/portfoliosite/controller/TestController.java
+++ src/main/java/com/pj/portfoliosite/portfoliosite/controller/TestController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    //ㅔㅌ스트 수정
     @GetMapping("/")
     public HttpEntity<String> index(){
         String response = "Hello World";
