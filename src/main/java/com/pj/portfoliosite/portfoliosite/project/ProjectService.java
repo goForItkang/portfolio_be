ce34403b62f13bd98c161ef734863078899e271e
goForItkang <kangyohan.dev.0421@gmail.com>
2025-08-21 17:12:07 +0900

    사용자 정보를 위해 가져오기위해 merge 작업 하기전에 push 함 -yh

--- src/main/java/com/pj/portfoliosite/portfoliosite/project/ProjectService.java
+++ src/main/java/com/pj/portfoliosite/portfoliosite/project/ProjectService.java
@@ -4,6 +4,7 @@ import com.pj.portfoliosite.portfoliosite.global.dto.ReqProject;
 import com.pj.portfoliosite.portfoliosite.global.dto.ResProjectRecommendDto;
 import com.pj.portfoliosite.portfoliosite.global.entity.Project;
 import com.pj.portfoliosite.portfoliosite.global.entity.User;
+import com.pj.portfoliosite.portfoliosite.user.UserService;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ProjectService {
     private final ProjectRepository projectRepository;
+    private final UserService userService; // userServiced;
     //추천 프로젝트 로직 오늘 부터 일주일 동안 가장 많은 좋아요 갯수
     public List<ResProjectRecommendDto> getRecommend() {
         //로직 오늘 날짜 부터 7일중 가장 좋아요 많은 프로젝트 12개 선정
