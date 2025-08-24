a8503ec4d3af37d3f528afca0ba7f5560457704d
goForItkang <kangyohan.dev.0421@gmail.com>
2025-08-24 21:01:47 +0900

    스웨거 security 적용안함 ㅠㅠ

--- src/main/java/com/pj/portfoliosite/portfoliosite/config/SecurityConfig.java
+++ src/main/java/com/pj/portfoliosite/portfoliosite/config/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig {
         http.cors(Customizer.withDefaults())
                 .csrf(csrf->csrf.disable())
                 .authorizeHttpRequests(auth->
-                        auth.requestMatchers("/","/test","/test2","/api/**").permitAll()
+                        auth.requestMatchers("/api-docs","/swagger-ui.html","/","/test","/test2","/api/**").permitAll()
                                 .anyRequest().authenticated()
                 )
                 .formLogin(form ->form
