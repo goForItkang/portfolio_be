b84369b81f150696af4a80fe01df0c6f42673768
goForItkang <kangyohan.dev.0421@gmail.com>
2025-09-25 21:29:11 +0900

    update -yh

--- src/main/java/com/pj/portfoliosite/portfoliosite/config/FileUploadConfig.java
+++ src/main/java/com/pj/portfoliosite/portfoliosite/config/FileUploadConfig.java
@@ -1,21 +1,21 @@
 package com.pj.portfoliosite.portfoliosite.config;
 
 
+import jakarta.servlet.MultipartConfigElement;
 import org.apache.tomcat.util.http.fileupload.FileUpload;
 import org.apache.tomcat.util.http.fileupload.disk.DiskFileItemFactory;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
 
 @Configuration
 public class FileUploadConfig {
     @Bean
-    public FileUpload customFileUpload() {
-        DiskFileItemFactory factory = new DiskFileItemFactory();
-        FileUpload upload = new FileUpload(); // 1. 기본 생성자로 객체를 생성합니다.
-        upload.setFileItemFactory(factory);   // 2. Setter를 사용해 factory를 설정합니다.
-        upload.setFileCountMax(30);
-
-
-        return upload;
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize(DataSize.ofMegabytes(20));      // 파일당 최대 20MB
+        factory.setMaxRequestSize(DataSize.ofMegabytes(100));  // 전체 요청 최대 100MB
+        return factory.createMultipartConfig();
     }
 }
