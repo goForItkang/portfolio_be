7b3ce4f8f0f101fe60c5a081f91b9cd55ce5e319
goForItkang <kangyohan.dev.0421@gmail.com>
2025-09-25 19:35:21 +0900

    git update -yh

--- src/main/java/com/pj/portfoliosite/portfoliosite/portfolio/PortFolioController.java
+++ src/main/java/com/pj/portfoliosite/portfoliosite/portfolio/PortFolioController.java
@@ -29,6 +29,7 @@ public class PortFolioController {
         DataResponse dataResponse = new DataResponse();
         dataResponse.setData(id);
         System.out.println("데이터 전을 받았습니당~");
+
         return ResponseEntity.ok(dataResponse);
     }
     @GetMapping("/portfolio")
