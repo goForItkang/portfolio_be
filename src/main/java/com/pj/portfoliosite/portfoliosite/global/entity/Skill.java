f71c853fee9e7d22eb085320925ead778d96fe42
goForItkang <kangyohan.dev.0421@gmail.com>
2025-09-27 20:33:07 +0900

    Skills getter 추가 -yh

--- src/main/java/com/pj/portfoliosite/portfoliosite/global/entity/Skill.java
+++ src/main/java/com/pj/portfoliosite/portfoliosite/global/entity/Skill.java
@@ -3,8 +3,10 @@ package com.pj.portfoliosite.portfoliosite.global.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.Getter;
 
 @Entity
+@Getter
 
 public class Skill {
     @Id
