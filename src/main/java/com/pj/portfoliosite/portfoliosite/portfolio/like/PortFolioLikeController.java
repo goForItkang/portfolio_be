3be7e0b11f99e3e50219f04cd5e0f36179ec4805
goForItkang <kangyohan.dev.0421@gmail.com>
2025-09-28 21:59:11 +0900

    portfolio/like log update -yh

--- src/main/java/com/pj/portfoliosite/portfoliosite/portfolio/like/PortFolioLikeController.java
+++ src/main/java/com/pj/portfoliosite/portfoliosite/portfolio/like/PortFolioLikeController.java
@@ -3,12 +3,14 @@ package com.pj.portfoliosite.portfoliosite.portfolio.like;
 import com.pj.portfoliosite.portfoliosite.global.dto.DataResponse;
 import com.pj.portfoliosite.portfoliosite.portfolio.PortFolioService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
+@Slf4j
 public class PortFolioLikeController {
     private final PortFolioLikeService portfolioLikeService;
     @PostMapping("/portfolio/{id}/like")
@@ -17,7 +19,7 @@ public class PortFolioLikeController {
     ){
         DataResponse dataResponse = new DataResponse();
         dataResponse.setData(null);
-
+        log.info("like id : " + id);
         dataResponse.setMessage("success");
         portfolioLikeService.portfolioLike(id);
         return ResponseEntity.ok(dataResponse);
