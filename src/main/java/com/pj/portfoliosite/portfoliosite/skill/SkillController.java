afa5d4533c89bbb5271b0847adbaa770e08547d5
goForItkang <kangyohan.dev.0421@gmail.com>
2025-09-29 19:33:22 +0900

    skills 해결-yh

--- src/main/java/com/pj/portfoliosite/portfoliosite/skill/SkillController.java
+++ src/main/java/com/pj/portfoliosite/portfoliosite/skill/SkillController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/skills")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class SkillController {
     
