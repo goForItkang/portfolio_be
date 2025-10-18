83a81a8d0f4b26e0a5c86214f7469b80e01fbac6
goForItkang <kangyohan.dev.0421@gmail.com>
2025-10-18 19:49:51 +0900

    bookmark user fix -yh

--- src/main/java/com/pj/portfoliosite/portfoliosite/blog/bookmark/BookmarkService.java
+++ src/main/java/com/pj/portfoliosite/portfoliosite/blog/bookmark/BookmarkService.java
@@ -5,7 +5,9 @@ import com.pj.portfoliosite.portfoliosite.global.entity.Blog;
 import com.pj.portfoliosite.portfoliosite.global.entity.BlogBookmark;
 import com.pj.portfoliosite.portfoliosite.global.entity.User;
 import com.pj.portfoliosite.portfoliosite.user.UserRepository;
+import com.pj.portfoliosite.portfoliosite.util.AESUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -16,10 +18,11 @@ public class BookmarkService {
     private final BookmarkRepository bookmarkRepository;
     private final UserRepository userRepository;
     private final BlogRepository blogRepository;
+    private final AESUtil aesUtil;
     public void save(Long id) {
         Blog blog = blogRepository.selectById(id);
-        String userEmail = "portfolio@naver.com";
-        Optional<User> user = userRepository.findByEmail(userEmail);
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        Optional<User> user = userRepository.findByEmail(aesUtil.encode(email));
         if(user.isPresent() ){
             BlogBookmark blogBookmark = new BlogBookmark();
             blogBookmark.addUser(user.get());
@@ -32,8 +35,8 @@ public class BookmarkService {
     }
 
     public void delete(Long id) {
-        String userEmail = "portfolio@naver.com";
-        Optional<User> user = userRepository.findByEmail(userEmail);
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        Optional<User> user = userRepository.findByEmail(aesUtil.encode(email));
         if(user.isPresent()){
             bookmarkRepository.delete(id,user.get().getId());
         }else{
